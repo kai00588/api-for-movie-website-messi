b06ce6dbafdd26ef5fbb5a3233131e5320efb717
kai00588 <61916927+kai00588@users.noreply.github.com>
2022-10-29 09:45:39 +0000

    third

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
+const cheerio = require('cheerio');
+app.use(cors())
 
-app.use(cors());
+const images = []
+const names = []
+const blogs = []
+async function scrapeData() {
+    
+    const {data} = await axios.get('https://allmovieshub.website/');
+    const $ = cheerio.load(data);
+    const movies = $("article .blog-entry-inner .thumbnail a img");
+    movies.each((index,el)=>{
+        images.push(el.attribs.src);
+        //console.log(el.attribs);
+    })
+    const titles = $("article .blog-entry-inner .blog-entry-header .blog-entry-title a")
+    titles.each((index,el)=>{
+        //console.log(el.children[0].data);
+        names.push(el.children[0].data);
+    })
+    while(blogs.length >0){
+        blogs.pop()
+    }
+    for (let index = 0; index < names.length; index++) {
+        const movie = {
+            'name': names[index],
+            'img': images[index]
+        }
+        
+        blogs.push(movie);
+    }
+}
 
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+    scrapeData();
+    res.send(blogs);
 })
 
 app.listen(port,()=>{
